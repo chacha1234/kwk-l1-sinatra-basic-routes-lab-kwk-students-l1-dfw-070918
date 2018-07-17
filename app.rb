ee46c5d0c405e3f66f1d6b62cb74d8b6eebc4d7c
Chamberlain Churchill <cbchurchill2004@icloud.com>
2018-07-17 16:49:49 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,15 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
 get '/name' do
   "My name is Chacha"
 end
 
-  get '/hometown' do
+get '/hometown' do
     "My hometown is Dallas"
 end
 
-  get '/favorite song' do
+get '/favorite song' do
     "My favorite song is In My Blood"
 end
 end
